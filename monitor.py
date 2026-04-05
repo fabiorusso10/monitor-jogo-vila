467e6aa48c9e1a5c41f8a23e245a5a6ee63839a5
FabioRusso10 <fabiorusso10@gmail.com>
2026-04-05 10:42:01 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,6 +6,8 @@ url = "https://www.jogueinavila.com.br"
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+ARQUIVO_ESTADO = "estado.txt"
+
 
 def enviar_telegram(mensagem):
     link = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
@@ -16,6 +18,19 @@ def enviar_telegram(mensagem):
     requests.post(link, data=data)
 
 
+def ja_enviado():
+    try:
+        with open(ARQUIVO_ESTADO, "r") as f:
+            return f.read().strip() == "enviado"
+    except:
+        return False
+
+
+def salvar_estado():
+    with open(ARQUIVO_ESTADO, "w") as f:
+        f.write("enviado")
+
+
 try:
     response = requests.get(url)
     content = response.text.lower()
@@ -23,16 +38,22 @@ try:
     if "novidades em breve" in content:
         print("⏳ Ainda não abriu")
     else:
-        print("🚨 MUDOU! Enviando alerta...")
+        print("🚨 MUDOU!")
 
-        mensagem = (
-            "🚨 ATENÇÃO!\n\n"
-            "O site do Jogue na Vila mudou!\n"
-            "Pode ter aberto inscrição!\n\n"
-            "👉 https://www.jogueinavila.com.br"
-        )
+        if not ja_enviado():
+            print("📩 Enviando alerta...")
 
-        enviar_telegram(mensagem)
+            mensagem = (
+                "🚨 ATENÇÃO!\n\n"
+                "O site do Jogue na Vila mudou!\n"
+                "Pode ter aberto inscrição!\n\n"
+                "👉 https://www.jogueinavila.com.br"
+            )
+
+            enviar_telegram(mensagem)
+            salvar_estado()
+        else:
+            print("🔁 Já avisado antes")
 
 except Exception as e:
     print(f"Erro: {e}")
