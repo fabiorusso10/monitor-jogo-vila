90b2910cc82b0fcfbe93e661ed25e8e78a372edf
FabioRusso10 <fabiorusso10@gmail.com>
2026-04-05 10:10:39 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,15 @@
-print("Monitor rodando com sucesso!")
+import requests
+
+url = "https://www.jogueinavila.com.br"  # depois a gente troca pelo site real
+
+try:
+    response = requests.get(url)
+    content = response.text.lower()
+
+    if "google" in content:
+        print("✅ Palavra encontrada! Site OK")
+    else:
+        print("❌ Palavra NÃO encontrada!")
+
+except Exception as e:
+    print(f"Erro ao acessar o site: {e}")
